6dfc5ed20fffce1fc298c2dd91d63e60fc6ca136
Aaron Flanagan <aaronflanagan044@hotmail.com>
2015-10-29 17:58:50 +0000

    Added help string and first query url

--- Server.js
+++ Server.js
@@ -11,18 +11,32 @@ var marData = JSON.parse(fs.readFileSync('Marriages 2011 - 2014.json','utf8'));
 
 //create http
 var app = express();
+//sets format for json
+app.set('json spaces', 1);
 
-//*******************MAIN SCREEN*******************//
+
+//*******************HELP STRINGS*******************//
 // When a user goes to /, return a small help string.
 app.get('/', function(req, res) {
-  res.send('Try http://127.0.0.1:8000/population');
+  res.send('Try http://127.0.0.1:8000/population/sex/age');
+});
+
+//when user goes to /population/sex/age instruct them what to add for json return
+app.get('/population/sex/age', function(req, res) {
+  res.send('add in to url /females or /males or /both for gender and an age /(1-98) or (99+)');
 });
 
 //*****************POPULATION GET REQUESTS******************//
-app.get('/population/sex/:sex', function(req, res) {
-  res.json(popData[req.params.sex]);
+//returns sex and age for all years
+app.get('/population/sex/age/:sex/:age', function(req, res) {
+	var results = [];
+	for(var i = 0; i < popData.length; i++)
+		if(popData[i].sex == req.params.sex)
+			if(popData[i].age == req.params.age)
+				results.push(popData[i]);
+	
+	res.json(results);
 });
-
 
 // Run the server
 var server = app.listen(8000);
